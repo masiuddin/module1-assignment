4b83a44c807025d53796f66a3e1342c83447e9fe
masi <mdmasiuddin43@gmail.com>
2016-09-19 00:17:51 -0500

    module1assignment

---
+++ app2.js
@@ -0,0 +1,17 @@
+(function(){
+
+  angular.module("LunchCheck",[])
+
+  .controller("LunchCheckController", lunchChecker);
+
+  lunchChecker.$inject = ['$scope'];
+  function lunchChecker($scope){
+    $scope.name = "";
+    $scope.message ="";
+    $scope.check = function(){
+
+      $scope.message = ($scope.name.split(",").length<=3)? "Enjoy!": "TOO much!";
+    };
+  }
+
+})();
