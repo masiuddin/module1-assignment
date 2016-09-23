502d81ba45791b07e60aafddf63c8adfaf8e09e0
masi <mdmasiuddin43@gmail.com>
2016-09-23 16:26:50 -0500

    assnmnt 1

--- app.js
+++ app.js
@@ -5,12 +5,12 @@
   .controller("LunchCheckController", lunchChecker);
 
   lunchChecker.$inject = ['$scope'];
+	
   function lunchChecker($scope){
     $scope.name = "";
     $scope.message ="";
     $scope.check = function(){
-
-      $scope.message = ($scope.name.split(",").length<=3)? "Enjoy!": "TOO much!";
+			$scope.message = ($scope.name.split(",").length<=3)? "Enjoy!": "TOO much!";
     };
   }
 
